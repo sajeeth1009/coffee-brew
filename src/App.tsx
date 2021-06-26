72fcba3b169938f722b004c71b1b5d2f6199b8ea
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-26 15:03:48 +0200

    introduce routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "./App.scss";
+import Brew from "./components/pages/Brew";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router
+      basename={
+        process.env.NODE_ENV === "production"
+          ? process.env.PUBLIC_URL
+          : undefined
+      }
+    >
+      <Switch>
+        <Route path="/brew">
+          <Brew />
+        </Route>
+        <Redirect to="/brew" />
+      </Switch>
+    </Router>
   );
 }
 
