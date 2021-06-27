3fa25edcd90167a5244355c58822f468cca277d4
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-27 19:35:37 +0200

    add back button to header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,17 +1,43 @@
 import clsx from "clsx";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import styles from "./Header.module.scss";
 
 interface HeaderProps {
   title?: string;
   caption?: string;
+  backEnabled?: boolean;
+  back?: () => void;
 }
 
 const Header: React.FC<HeaderProps> = (props) => {
+  const renderBackButton = () => {
+    return (
+      <span
+        className={clsx(
+          styles.backIcon,
+          "d-flex align-items-center justify-content-center"
+        )}
+        onClick={props.back}
+      >
+        <FontAwesomeIcon size="1x" icon={faChevronLeft}></FontAwesomeIcon>
+      </span>
+    );
+  };
+
   return (
     <div>
       <div className="container p-2">
-        <p className={clsx(styles.title, "m-0 fw-bold")}>{props.title}</p>
+        <p
+          className={clsx(
+            styles.title,
+            "d-flex align-items-center m-0 fw-bold"
+          )}
+        >
+          {props.backEnabled ? renderBackButton() : null}
+          <span>{props.title}</span>
+        </p>
         <p className={clsx(styles.caption, "m-0")}>{props.caption}</p>
       </div>
     </div>
