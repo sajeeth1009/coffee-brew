0b413e3274253be8516feb3c1695bad6701f7b78
sajeeth1009 <sajeeth.sadanand@gmail.com>
2021-06-27 19:36:31 +0200

    add type for final coffee order

--- src/api/types/coffeeMenu.ts
+++ src/api/types/coffeeMenu.ts
@@ -1,29 +1,35 @@
 export interface CoffeeMenu {
-  id: string;
+  _id: string;
   types: CoffeeType[];
   sizes: CoffeeSize[];
   extras: CoffeeExtras[];
 }
 
 export interface CoffeeType {
-  id: string;
+  _id: string;
   name: string;
   sizes: string[];
   extras: string[];
 }
 
 export interface CoffeeSize {
-  id: string;
+  _id: string;
   name: string;
 }
 
 export interface CoffeeExtras {
-  id: string;
+  _id: string;
   name: string;
   subselections: SubSelection[];
 }
 
 export interface SubSelection {
-  id: string;
+  _id: string;
   name: string;
+}
+
+export interface SelectedCoffeeExtra {
+  _id: string;
+  name: string;
+  subselections: SubSelection;
 }
